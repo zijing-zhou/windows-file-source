179410d73b14a6aad3ba880d744a0e425bbc0af2
zijing.zhou <zijing.zhou@aliyun.com>
2025-06-22 23:01:51 +0800

    Feature 2: Downlaod ISO File

--- src/tests/main.py
+++ src/tests/main.py
@@ -1,5 +1,8 @@
 from enum_handler import ISORequestHandler
+import time
 
 if __name__ == "__main__":
     handler = ISORequestHandler(db_path="iso.db")
-    handler.process_requests()
+    while True:
+        handler.process_requests()
+        time.sleep(10)
