e5347e036986708981b46988c42fdd91587481c8
zijing.zhou <zijing.zhou@aliyun.com>
2025-06-24 23:55:29 +0800

    Feature 2: Downlaod ISO File

--- src/downloader/magnet_downloader.py
+++ src/downloader/magnet_downloader.py
@@ -14,13 +14,11 @@ class MagnetDownloader:
         os.makedirs(save_path, exist_ok=True)
         ses = lt.session()
         ses.listen_on(6881, 6891)
-
         params = {
             'save_path': save_path,
             'storage_mode': lt.storage_mode_t.storage_mode_sparse,
         }
         handle = lt.add_magnet_uri(ses, magnet_link, params)
-
         print("Waiting for metadata...")
         while not handle.has_metadata():
             time.sleep(1)
@@ -29,5 +27,4 @@ class MagnetDownloader:
             status = handle.status()
             print(f"{status.progress * 100:.2f}% done. Download rate: {status.download_rate / 1000:.2f} kB/s")
             time.sleep(5)
-
         return unquote(self._extract_filename(handle))
