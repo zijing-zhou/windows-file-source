3a5a8131b80e2a0cbb9ef45a5465a4f90a026181
zijing-zhou <zijing.zhou@aliyun.com>
2025-09-14 22:55:01 +0800

    Update virtualbox_manager.py

--- src/vm/virtualbox_manager.py
+++ src/vm/virtualbox_manager.py
@@ -1,28 +1,17 @@
 
-import os
-import subprocess
-import time
-import shutil
-import json
-from vboxapi import VirtualBoxManager
+import vboxshell
+
 
 class VirtualBoxManager:
-    def create_windows_vm(self, vm_name, os_type_id, vm_memory_size):
-        vbox_mgr = VirtualBoxManager(None, None)
-        vbox = vbox_mgr.mgr.getVirtualBox()
-        session = vbox_mgr.mgr.getSessionObject(vbox)
-        sFlags = ''
-        sCipher = '' ## @todo No encryption support here yet!
-        sPasswordID = ''
-        sPassword = ''
-        # TODO: amd64: 1
-        machine = vbox.createMachine("", vm_name, , [], os_type_id, sFlags, sCipher, sPasswordID, sPassword)
-        machine.saveSettings()
-        vbox.registerMachine(machine)
-        machine.lockMachine(session, vbox_mgr.constants.LockType_Write)
-        try:
-            session.machine.memorySize = vm_memory_size
-            session.machine.saveSettings()
-        finally:
-            session.unlockMachine()
-        vbox_mgr = None
+    ctx = None
+
+    def __init__(ctx):
+        self.ctx = ctx
+    
+    def create_windows_vm(self, name, arch, kind, memerysize):
+        vboxshell.createVm(self.ctx, name, arch, kind, memerysize)
+        
+    def start_vm(self, ctx, mach, vmtype):
+        vboxshell.startVm(self.ctx, mach, vmtype)
+
+
