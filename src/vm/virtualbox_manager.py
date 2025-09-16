ac3179ed02b49622336d9f72a4a5b8d5729f4f43
zijing-zhou <zijing.zhou@aliyun.com>
2025-09-16 22:06:36 +0800

    Update virtualbox_manager.py

--- src/vm/virtualbox_manager.py
+++ src/vm/virtualbox_manager.py
@@ -1,5 +1,8 @@
 from vboxapi import VirtualBoxManager
 from vboxshell import createVm
+from vboxshell import startVm
+from vboxshell import removeVm
+from vboxshell import argsToMach
 
 class VirtualBox:
     def __init__(self, ctx=None):
@@ -13,3 +16,11 @@ class VirtualBox:
 
     def create_windows_vm(self, name, arch, kind):
         createVm(self.ctx, name, arch, kind)
+
+    def start_windows_vm(self, name):
+        mach = argsToMach(self.ctx, name)
+        startVm(self.ctx, mach, "gui")
+
+    def remove_windows_vm(self, name):
+        mach = argsToMach(self.ctx, name)
+        removeVm(self.ctx, mach)
