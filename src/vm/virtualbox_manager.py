ceb62624bc079bacda97e383c681f3d3737a958a
zijing-zhou <zijing.zhou@aliyun.com>
2025-10-15 22:40:45 +0800

    Update virtualbox_manager.py

--- src/vm/virtualbox_manager.py
+++ src/vm/virtualbox_manager.py
@@ -3,6 +3,7 @@ from vm.vboxshell import createVm
 from vm.vboxshell import startVm
 from vm.vboxshell import removeVm
 from vm.vboxshell import argsToMach
+from vm.vboxshell import getMachines
 
 class VirtualBox:
     def __init__(self, ctx=None):
@@ -16,32 +17,64 @@ class VirtualBox:
             'perf': None
         }
         self.ctx = ctx
+        self.vbox_mgr = vbox_mgr
                 
     def create_windows_vm(self, name, arch, kind):
         if '_machlist' not in self.ctx:
             self.ctx['_machlist'] = None
-        createVm(self.ctx, name, arch, kind)
+        createVm(self.ctx, name, arch, kind)        
 
     def start_windows_vm(self, name):
-        mach = argsToMach(self.ctx, name)
-        startVm(self.ctx, mach, "gui")
+        uuid = self.getUUIDByName(name)
+        if uuid is not None:
+            mach = argsToMach(self.ctx, [name, uuid])
+            startVm(self.ctx, mach, "gui")
 
     def poweroff_windows_vm(self, name):
-        mach = argsToMach(self.ctx, name)
+        uuid = self.getUUIDByName(name)
+        if uuid is not None:
+            mach = argsToMach(self.ctx, [name, uuid])
         pass
 
     def backup_windows_vm(self, name):
-        mach = argsToMach(self.ctx, name)
+        uuid = self.getUUIDByName(name)
+        if uuid is not None:
+            mach = argsToMach(self.ctx, [name, uuid])
         pass
 
     def restore_windows_vm(self, name):
-        mach = argsToMach(self.ctx, name)
+        uuid = self.getUUIDByName(name)
+        if uuid is not None:
+            mach = argsToMach(self.ctx, [name, uuid])
         pass
         
     def remove_windows_vm(self, name):
-        mach = argsToMach(self.ctx, name)
-        removeVm(self.ctx, mach)
+        uuid = self.getUUIDByName(name)
+        if uuid is not None:
+            mach = argsToMach(self.ctx, [name, uuid])
+            removeVm(self.ctx, mach)        
         
+    def set_vm_resources(self, name, cpu_count: int = 2, memory_gb: int = 4):
+        vbox = self.ctx['vb']
+        uuid = self.getUUIDByName(name)
+        if uuid is not None:
+            mach = argsToMach(self.ctx, [name, uuid])
+            session = self.ctx['global'].openMachineSession(mach, fPermitSharing=True)
+            mach = session.machine
+            try:
+                m = mach
+                m.MemorySize = memory_gb * 1024
+                m.CPUCount = cpu_count
+                m.saveSettings()
+            finally:
+                session.unlockMachine()
 
-
+    def getUUIDByName(self, name):
+        for mach in getMachines(self.ctx, True):
+            try:
+                if mach.name == name:
+                    return mach.id
+            except Exception as e:
+                return None
+        return None
 
